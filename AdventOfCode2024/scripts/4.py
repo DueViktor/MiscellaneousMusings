33c0719f370a4be29f1e7baf8f69086a5a879011
DueViktor <viktorduep@gmail.com>
2024-12-08 14:42:04 +0100

    done 4

--- AdventOfCode2024/scripts/4.py
+++ AdventOfCode2024/scripts/4.py
@@ -1,3 +1,5 @@
+"""Part one"""
+
 import numpy as np
 
 # Read the matrix from the file
@@ -36,6 +38,41 @@ def xmas(matrix, ridx, cidx) -> int:
     if ridx + L <= rows and "".join(matrix[ridx : ridx + L, cidx]) == TARGET_WORD:
         count += 1
 
+    # diagonal down right
+    if (
+        ridx + L <= rows
+        and cidx + L <= cols
+        and "".join(matrix[range(ridx, ridx + L), range(cidx, cidx + L)]) == TARGET_WORD
+    ):
+        count += 1
+
+    # diagonal down left
+    if (
+        ridx + L <= rows
+        and cidx - L + 1 >= 0
+        and "".join(matrix[range(ridx, ridx + L), range(cidx, cidx - L, -1)])
+        == TARGET_WORD
+    ):
+        count += 1
+
+    # diagonal up left
+    if (
+        ridx - L + 1 >= 0
+        and cidx - L + 1 >= 0
+        and "".join(matrix[range(ridx, ridx - L, -1), range(cidx, cidx - L, -1)])
+        == TARGET_WORD
+    ):
+        count += 1
+
+    # diagonal up right
+    if (
+        ridx - L + 1 >= 0
+        and cidx + L <= cols
+        and "".join(matrix[range(ridx, ridx - L, -1), range(cidx, cidx + L)])
+        == TARGET_WORD
+    ):
+        count += 1
+
     return count
 
 
@@ -44,4 +81,31 @@ for ridx in range(rows):
     for cidx in range(cols):
         total += xmas(matrix, ridx, cidx)
 
-print("Total occurrences of TARGET_WORD:", total)
+print(total)
+
+"""Part two
+Det er så lol. 
+"""
+
+
+def three_x_three(matrix, ridx, cidx):
+    """Extract and return the 3x3 grid starting at (ridx, cidx)."""
+    return matrix[ridx : ridx + 3, cidx : cidx + 3]
+
+
+def mas(subgrid) -> int:
+    target = "MAS"
+    top_left_bottom_right = "".join(subgrid[0, 0] + subgrid[1, 1] + subgrid[2, 2])
+    top_right_bottom_left = "".join(subgrid[0, 2] + subgrid[1, 1] + subgrid[2, 0])
+
+    return (
+        top_left_bottom_right == target or top_left_bottom_right[::-1] == target
+    ) and (top_right_bottom_left == target or top_right_bottom_left[::-1] == target)
+
+
+total = 0
+for ridx in range(rows - 2):
+    for cidx in range(cols - 2):
+        subgrid = three_x_three(matrix, ridx, cidx)
+        total += mas(subgrid)
+print(total)
